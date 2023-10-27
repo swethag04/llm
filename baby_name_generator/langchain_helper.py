b39b0966af5d26f5f46c901ea85a985b89d5785f
Swetha Ganapathi Raman <swethag04@gmail.com>
2023-10-27 09:38:29 -0700

    initial commit
    initial commit

---
+++ baby_name_generator/langchain_helper.py
@@ -0,0 +1,35 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from secret_key import openapi_key
+
+
+import os
+os.environ['OPENAI_API_KEY'] = openapi_key
+
+llm = OpenAI(temperature = 0.6)
+name = llm("Suggest top 5 popular names of American baby girls.")
+print(name)
+
+def generate_baby_names(gender,nationality):
+    prompt_template_name = PromptTemplate(
+        input_variables=['gender', 'nationality'],
+        template="""I want to find a name for a {nationality} {gender} baby. Suggest top 5 popular names for the baby.
+                   Return it as a comma separated list """)
+    name_chain = LLMChain(llm=llm,
+                          prompt=prompt_template_name,
+                          output_key='baby_names')
+
+    chain = SequentialChain(
+        chains=[name_chain],
+        input_variables=['gender', 'nationality'],
+        output_variables=['baby_names']
+    )
+
+    response = chain({'gender': gender,
+                      'nationality': nationality})
+    return response
+
+if __name__ == "__main__":
+    print(generate_baby_names("Indian"))
---
+++ baby_name_generator/secret_key.py
@@ -0,0 +1,1 @@
+openapi_key = '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'
